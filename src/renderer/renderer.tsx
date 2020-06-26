9a9e30eb691c0a41b8908f00eebb789b56cd8d1d
Rubinder Singh <mail2rubinder@gmail.com>
2020-06-26 21:53:03 +1000

    intial commit

---
+++ src/renderer/renderer.tsx
@@ -0,0 +1,16 @@
+/**
+ * React renderer.
+ */
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+// Import the styles here to process them with webpack
+import '@public/style.css';
+
+ReactDOM.render(
+  <div className="app">
+    <h4>Welcome to React, Electron and Typescript 3</h4>
+    <p>Hello</p>
+  </div>,
+  document.getElementById('app'),
+);
