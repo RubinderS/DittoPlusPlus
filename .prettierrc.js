bc29d471184dc2e4991b57de7581f617676cea3a
Rubinder Singh <mail2rubinder@gmail.com>
2020-07-19 18:03:40 +1000

    added prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  bracketSpacing: false,
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: 'always',
+  singleQuote: true,
+  trailingComma: 'all',
+};
