932074e5509e9eadb449cf288b634596d96faf4a
Rubinder Singh <mail2rubinder@gmail.com>
2020-11-29 13:49:36 +1100

    refactored clip item selection process

--- src/renderer/plugins/clipboard/render.tsx
+++ src/renderer/plugins/clipboard/render.tsx
@@ -1,14 +1,69 @@
 import {Box} from '@material-ui/core';
 import * as React from 'react';
 import {Theme, createStyles, makeStyles} from '@material-ui/core';
+import {grey} from '@material-ui/core/colors';
 import {ClipItem, Events, Messages} from './types';
 import * as PluginTypes from '@type/pluginTypes';
 import {useEffect, useState} from 'react';
+import useEventListener from '@use-it/event-listener';
+import * as _ from 'lodash';
 
 export const ClipboardComponent = (props: PluginTypes.RenderProps) => {
   const classes = useStyles();
   const [clipItems, updateClipItems] = useState<ClipItem[]>([]);
+  const [selectedIndex, updateSelectedIndex] = useState(0);
   const {process} = props;
+
+  const reArrangeClipItems = (selectedClipItem: ClipItem) => {
+    const index = clipItems.findIndex(
+      (clipItem) => clipItem._id === selectedClipItem._id,
+    );
+
+    const slicedClipItems = [
+      ...clipItems.slice(0, index),
+      ...clipItems.slice(index + 1),
+    ];
+
+    updateClipItems([selectedClipItem, ...slicedClipItems]);
+  };
+
+  const sendClipboardItemSelected = (clipItem: ClipItem) => {
+    if (clipItem.type === 'text') {
+      process.sendMessage(Messages.WriteClipText, clipItem, (err, res) => {
+        if (err) {
+          throw err;
+        }
+
+        if (res) {
+          // console.log(res);
+        }
+      });
+    }
+  };
+
+  const onKeyPress = (event: KeyboardEvent) => {
+    const {key} = event;
+
+    switch (key) {
+      case 'ArrowUp':
+        updateSelectedIndex((prevSelectedIndex) =>
+          _.clamp(prevSelectedIndex - 1, 0, clipItems.length - 1),
+        );
+        break;
+
+      case 'ArrowDown':
+        updateSelectedIndex((prevSelectedIndex) =>
+          _.clamp(prevSelectedIndex + 1, 0, clipItems.length - 1),
+        );
+        break;
+
+      case 'Enter':
+        //
+        break;
+    }
+  };
+
+  useEventListener('keydown', onKeyPress);
 
   useEffect(() => {
     process.sendMessage(Messages.GetAllClipItems, '', (err, res) => {
@@ -27,23 +82,17 @@ export const ClipboardComponent = (props: PluginTypes.RenderProps) => {
   }, []);
 
   const onClickClipItem = (e: ClipItem) => {
-    const index = clipItems.findIndex((clipItem) => clipItem._id === e._id);
-    const slicedClipItems = [
-      ...clipItems.slice(0, index),
-      ...clipItems.slice(index + 1),
-    ];
-    updateClipItems([e, ...slicedClipItems]);
+    reArrangeClipItems(e);
+    sendClipboardItemSelected(e);
+  };
 
-    if (e.type === 'text') {
-      process.sendMessage(Messages.WriteClipText, e, (err, res) => {
-        if (err) {
-          throw err;
-        }
-
-        if (res) {
-          // console.log(res);
-        }
-      });
+  const getBackgroundColor = (index: number, selectedIndex: number) => {
+    if (index === selectedIndex) {
+      return grey[400];
+    } else if (index % 2) {
+      return grey[50];
+    } else {
+      return grey[200];
     }
   };
 
@@ -53,7 +102,7 @@ export const ClipboardComponent = (props: PluginTypes.RenderProps) => {
         <Box
           key={`${index}_clipItem`}
           className={classes.clipItem}
-          style={{backgroundColor: index % 2 ? '#F3F3F3' : '#CCCCCC'}}
+          style={{backgroundColor: getBackgroundColor(index, selectedIndex)}}
           onClick={() => onClickClipItem(item)}
         >
           {item.data}
